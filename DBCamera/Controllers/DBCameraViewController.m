058bf851fd0aa8b8f6d3b354b0a8497f37d8bd73
Sebastian Ludwig <sebastian@lurado.de>
2015-05-10 16:56:38 +0200

    Inlined misspelled method that has only been used in one place.

--- DBCamera/Controllers/DBCameraViewController.m
+++ DBCamera/Controllers/DBCameraViewController.m
@@ -275,13 +275,6 @@ NSLocalizedStringFromTable(key, @"DBCamera", nil)
     }
 }
 
-- (void) disPlayGridViewToCameraView:(BOOL)show
-{
-    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        self.cameraGridView.alpha = (show ? 1.0 : 0.0);
-    } completion:NULL];
-}
-
 #pragma mark - CameraManagerDelagate
 
 - (void) closeCamera
@@ -296,7 +289,9 @@ NSLocalizedStringFromTable(key, @"DBCamera", nil)
 }
 
 - (void) cameraView:(UIView *)camera showGridView:(BOOL)show {
-    [self disPlayGridViewToCameraView:!show];
+    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.cameraGridView.alpha = (show ? 1.0 : 0.0);
+    } completion:NULL];
 }
 
 - (void) triggerFlashForMode:(AVCaptureFlashMode)flashMode
